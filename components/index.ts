f401cc52018b4540daf81812d4ee46e7f71469d3
Newman Ferrer <newmanferrer@gmail.com>
2023-02-25 18:41:07 -0400

    feat: :sparkles: add component NavBarTest
    add component NavBarTest
    
    issue 250223 - add component NavBarTest

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,14 @@
 export { Header } from './Header/Header'
 export { Logo } from './Logo/Logo'
+
 export { Navigation } from './Navigation/Navigation'
+export { NavigationMobile } from './NavigationMobile/NavigationMobile'
+
 export { HamburgerIcon } from './HamburgerIcon/HamburgerIcon'
 export { CloseIcon } from './CloseIcon/CloseIcon'
+
 export { CartButton } from './CartButton/CartButton'
-export { LightDarkButton } from './LightDarkButton/LightDarkButton'
 export { Languages } from './Languages/Languages'
+export { LightDarkButton } from './LightDarkButton/LightDarkButton'
+
+export { NavBarTest } from './NavBarTest/NavBarTest'
