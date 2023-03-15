0a8d97298801f919af89e3f329a723b91c55743c
Newman Ferrer <newmanferrer@gmail.com>
2023-03-15 18:45:49 -0400

    refactor: :recycle: refactor services
    refactor services
    
    issue - 150323 - refactor services

--- services/products/products.ts
+++ services/products/products.ts
@@ -4,40 +4,42 @@ import { IProduct, IProductCategory, IProductTag } from '@/models'
 //* 1.- All Products
 //* *************************************************************************************
 //* -------------------------------------------------------------------------------------
-//* 1.1.- Generate static HTML
+//* 1.1.- Generate static HTML: OK
 //* -------------------------------------------------------------------------------------
 //* Default: It is equivalent to the old "getStaticProps()" in Nextjs 12
 //* Note: It is not necessary to put the parameter: "{ cache: 'force-cache' }"
 //* -------------------------------------------------------------------------------------
 const getAllProductsStatic = async (): Promise<IProduct[]> => {
-  return await fetch(`${process.env.BASE_URL}/api/get.all.products`).then(response =>
-    response.json()
-  )
+  return await fetch(`${process.env.BASE_URL}/api/products/get.all.products`, {
+    cache: 'force-cache'
+  }).then(response => response.json())
 }
 //* -------------------------------------------------------------------------------------
 
 //* -------------------------------------------------------------------------------------
-//* 1.2.- Generate Dynamic HTML if receive a request
+//* 1.2.- Generate Dynamic HTML if receive a request: OK
 //* -------------------------------------------------------------------------------------
 //* It is equivalent to the old "getServerSideProps()" in Nextjs 12
 //* -------------------------------------------------------------------------------------
 const getAllProductsDynamic = async (): Promise<IProduct[]> => {
-  return await fetch(`${process.env.BASE_URL}/api/get.all.products`, { cache: 'no-store' }).then(
-    response => response.json()
-  )
+  return await fetch(`${process.env.BASE_URL}/api/products/get.all.products`, {
+    cache: 'no-store'
+  }).then(response => response.json())
 }
 //* -------------------------------------------------------------------------------------
 
 //* -------------------------------------------------------------------------------------
-//* 1.3.- Generate Increment HTML - Revalidate Data if receive a request
+//* 1.3.- Generate Increment HTML - Revalidate Data if receive a request: OK
 //* -------------------------------------------------------------------------------------
 //* It is equivalent to the old (ISR) "Incremental Static Regeneration" in Nextjs 12
 //* Note: revalidate in seconds
 //* -------------------------------------------------------------------------------------
 const getAllProductsIncrement = async (): Promise<IProduct[]> => {
-  return await fetch(`${process.env.BASE_URL}/api/get.all.products`, {
+  return await fetch(`${process.env.BASE_URL}/api/products/get.all.products`, {
     next: { revalidate: 60 }
-  }).then(response => response.json())
+  })
+    .then(response => response.json())
+    .then(dataJson => dataJson.products)
 }
 //* -------------------------------------------------------------------------------------
 //* *************************************************************************************
@@ -46,38 +48,40 @@ const getAllProductsIncrement = async (): Promise<IProduct[]> => {
 //* 2.- Latest Products
 //* *************************************************************************************
 //* -------------------------------------------------------------------------------------
-//* 2.1.- Generate static HTML
+//* 2.1.- Generate static HTML: OK
 //* -------------------------------------------------------------------------------------
 //* Default: It is equivalent to the old "getStaticProps()" in Nextjs 12
 //* Note: It is not necessary to put the parameter: "{ cache: 'force-cache' }"
 //* -------------------------------------------------------------------------------------
-const getAllLatestProductsStatic = async (): Promise<IProduct[]> => {
-  return await fetch(`${process.env.BASE_URL}/api/get.all.latest.products`).then(response =>
-    response.json()
-  )
+const getAllLatestProductsStatic = async (quantity: number = 3): Promise<IProduct[]> => {
+  return await fetch(`${process.env.BASE_URL}/api/products/get.all.latest.products`, {
+    cache: 'force-cache'
+  })
+    .then(response => response.json())
+    .then(response => response.slice(0, quantity))
 }
 //* -------------------------------------------------------------------------------------
 
 //* -------------------------------------------------------------------------------------
-//* 2.2.- Generate Dynamic HTML if receive a request
+//* 2.2.- Generate Dynamic HTML if receive a request: OK
 //* -------------------------------------------------------------------------------------
 //* It is equivalent to the old "getServerSideProps()" in Nextjs 12
 //* -------------------------------------------------------------------------------------
 const getAllLatestProductsDynamic = async (): Promise<IProduct[]> => {
-  return await fetch(`${process.env.BASE_URL}/api/get.all.latest.products`, {
+  return await fetch(`${process.env.BASE_URL}/api/products/get.all.latest.products`, {
     cache: 'no-store'
   }).then(response => response.json())
 }
 //* -------------------------------------------------------------------------------------
 
 //* -------------------------------------------------------------------------------------
-//* 2.3.- Generate Increment HTML - Revalidate Data if receive a request
+//* 2.3.- Generate Increment HTML - Revalidate Data if receive a request: OK
 //* -------------------------------------------------------------------------------------
 //* It is equivalent to the old (ISR) "Incremental Static Regeneration" in Nextjs 12
 //* Note: revalidate in seconds
 //* -------------------------------------------------------------------------------------
 const getAllLatestProductsIncrement = async (): Promise<IProduct[]> => {
-  return await fetch(`${process.env.BASE_URL}/api/get.all.latest.products`, {
+  return await fetch(`${process.env.BASE_URL}/api/products/get.all.latest.products`, {
     next: { revalidate: 60 }
   }).then(response => response.json())
 }
