586135c8b86c03b4dcbea91101e0dd4de596bc29
Newman Ferrer <newmanferrer@gmail.com>
2023-03-15 18:42:02 -0400

    feat: :sparkles: create new route handlers: "get.all.products" and "get.all.latest.products"
    create new route handlers: "get.all.products" and "get.all.latest.products"
    
    issue - 150323 - create new route handlers: "get.all.products" and "get.all.latest.products"

---
+++ app/api/products/get.all.latest.products/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/lib'
+
+//* Generate static HTML (Static Route Handlers)
+export async function GET() {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        tags: {
+          some: {
+            name: {
+              contains: 'latest'
+            }
+          }
+        }
+      }
+    })
+    return NextResponse.json(products, { status: 200, statusText: 'GET OK' })
+  } catch (error) {
+    return NextResponse.json(error, { status: 500, statusText: 'GET ERROR: Internal Server Error' })
+  }
+}
