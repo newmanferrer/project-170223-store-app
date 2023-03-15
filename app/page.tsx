2a49ead340d14f8b8a09b7dc71e8932ca09b9f8d
Newman Ferrer <newmanferrer@gmail.com>
2023-03-15 18:58:03 -0400

    fix: :bug: TypeScritp Error: TS(2786): @ts-expect-error Async Server Component
    TypeScritp Error: TS(2786): @ts-expect-error Async Server Component
    
    issue - 150323 - TypeScritp Error: TS(2786): @ts-expect-error Async Server Component

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ export default async function HomePage() {
   return (
     <div className={styles.container}>
       <Header bgTransparent />
-
       <SectionBanner />
+
+      {/* https://beta.nextjs.org/docs/data-fetching/fetching */}
+      {/* @ts-expect-error Async Server Component */}
       <SectionLatestProducts title='Latest Products' subtitle='These are our latest products' />
 
       <Footer />
