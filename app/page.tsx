98c704358e6ac5bc92d295b6d27cbcb906c22da8
Newman Ferrer <newmanferrer@gmail.com>
2023-03-16 03:34:13 -0400

    feat: :sparkles: add new section products
    add new section products
    
    issue - 160323 - add new section products

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import { Header, SectionBanner, SectionLatestProducts, Footer } from '../components'
+import { Header, SectionBanner, SectionProducts, Footer } from '../components'
+import { services } from '@/services'
 import styles from './page.module.scss'
 
 export default async function HomePage() {
@@ -9,7 +10,29 @@ export default async function HomePage() {
 
       {/* https://beta.nextjs.org/docs/data-fetching/fetching */}
       {/* @ts-expect-error Async Server Component */}
-      <SectionLatestProducts title='Latest Products' subtitle='These are our latest products' />
+      <SectionProducts
+        title='Latest Products'
+        subtitle='These are our latest products'
+        productQuantity={3}
+        serviceFunction={services.getAllLatestProductsStatic}
+      />
+
+      {/* @ts-expect-error Async Server Component */}
+      <SectionProducts
+        title='New Arrivals'
+        subtitle='Look at our next products to arrive for you'
+        productQuantity={3}
+        serviceFunction={services.getAllNewArrivalsProductsStatic}
+        backgroundDark
+      />
+
+      {/* @ts-expect-error Async Server Component */}
+      <SectionProducts
+        title='Most Wanted'
+        subtitle='These are the products most sought after by our customers'
+        productQuantity={3}
+        serviceFunction={services.getAllMostWantedProductsStatic}
+      />
 
       <Footer />
     </div>
