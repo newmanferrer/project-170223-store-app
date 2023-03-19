b1629da1ce0f607a3c443429729f65ce7b5bc991
Newman Ferrer <newmanferrer@gmail.com>
2023-03-19 19:22:54 -0400

    feat: :sparkles: create new function "getProductsByTag"
    create new function "getProductsByTag"
    
    issue - 190323 - create new function "getProductsByTag"

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,10 @@
 export { prisma } from './prisma/db/client'
-export { getAllProducts, getProductById, createProduct } from './prisma/products/products'
+
+export {
+  getProducts,
+  getProductById,
+  getProductsByTag,
+  createProduct
+} from './prisma/products/products'
 
 export { convertProductNameToPath, createFriendlyUrl } from './utils'
--- lib/prisma/products/products.ts
+++ lib/prisma/products/products.ts
@@ -1,11 +1,12 @@
 import { prisma } from '@/lib'
 import { type Product } from '@prisma/client'
 
-export async function getAllProducts() {
+export async function getProducts() {
   try {
     const products = await prisma.product.findMany({ include: { categories: true, tags: true } })
     return { products }
   } catch (error) {
+    console.error(error)
     const ErrorMessage = 'Error: lib/prisma/products/products.ts -> "getAllProducts"'
     return { ErrorMessage }
   }
@@ -20,6 +21,30 @@ export async function getProductById(productId: string) {
     return { product }
   } catch (error) {
     const ErrorMessage = 'Error: lib/prisma/products/products.ts -> "getProductById"'
+    return { ErrorMessage }
+  }
+}
+
+export async function getProductsByTag(productTag: string) {
+  try {
+    const products = await prisma.product.findMany({
+      where: {
+        tags: {
+          some: {
+            name: {
+              contains: productTag
+            }
+          }
+        }
+      },
+      include: {
+        categories: true,
+        tags: true
+      }
+    })
+    return { products }
+  } catch (error) {
+    const ErrorMessage = 'Error: lib/prisma/products/products.ts -> "getLatestProducts"'
     return { ErrorMessage }
   }
 }
