d6c2104be3f99e9f0082ad7777367f3b22b71ae1
Newman Ferrer <newmanferrer@gmail.com>
2023-03-25 11:08:29 -0400

    feat: :sparkles: create new component "AddToCartButton.tsx" and Styles
    create new component "AddToCartButton.tsx" and Styles
    
    issue - 250323 - create new component "AddToCartButton.tsx" and Styles

---
+++ components/AddToCartButton/AddToCartButton.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Product } from '@prisma/client'
+import { useShoppingCartContext } from '@/contexts'
+import styles from './AddToCartButton.module.css'
+
+interface IAddToCartButtonProps {
+  product: Product
+}
+
+export const AddToCartButton = ({ product }: IAddToCartButtonProps) => {
+  const { addProductToCart, openCart } = useShoppingCartContext()
+
+  const handleClick = () => {
+    addProductToCart(product)
+    openCart()
+  }
+
+  return (
+    <button className={styles.button} onClick={handleClick}>
+      Add to Cart
+    </button>
+  )
+}
