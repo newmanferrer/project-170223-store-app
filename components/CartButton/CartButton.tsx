daf4ff31a23eb540a281262745842843ae254548
Newman Ferrer <newmanferrer@gmail.com>
2023-03-27 11:02:58 -0400

    feat: :sparkles: create function "handleClick" for close navigation mobile and toggle shopping cart
    create function "handleClick" for close navigation mobile and toggle shopping cart
    
    issue - 270323 - create function "handleClick" for close navigation mobile and toggle shopping cart

--- components/CartButton/CartButton.tsx
+++ components/CartButton/CartButton.tsx
@@ -1,13 +1,19 @@
 import Image from 'next/image'
-import { useShoppingCartContext } from '@/contexts'
+import { useShoppingCartContext, useNavigationMobileContext } from '@/contexts'
 import styles from './CartButton.module.scss'
 import ShoppingCartIcon from '../../public/images/shopping.cart.icons/cart.solid.svg'
 
 export function CartButton() {
   const { toggleCart, getNumberOfProducts } = useShoppingCartContext()
+  const { closeNavigationMobile } = useNavigationMobileContext()
+
+  const handleClick = () => {
+    toggleCart()
+    closeNavigationMobile()
+  }
 
   return (
-    <div className={styles.container} onClick={toggleCart}>
+    <div className={styles.container} onClick={handleClick}>
       <Image
         className={styles.container__image}
         src={ShoppingCartIcon}
