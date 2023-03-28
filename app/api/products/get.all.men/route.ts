1459d8db52a56f579e4bd40fee40376b1aaf2c65
Newman Ferrer <newmanferrer@gmail.com>
2023-03-28 19:38:31 -0400

    feat: :sparkles: create news api route
    create news api route
    
    issue - 20323 - create news api route

---
+++ app/api/products/get.all.men/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getProductsByTag } from '@/lib'
+
+//* =====================================================================================
+//* Generate static HTML (Static Route Handlers) - OK
+//* =====================================================================================
+//* Route Handlers are statically evaluated by default when using the GET method
+//* with the Response object.
+//* It is equivalent to the old "getStaticProps()" in Nextjs 12
+//* It is equivalent to put the parameter: "{ cache: 'force-cache' }" in Nextjs 13.0
+//* =====================================================================================
+export async function GET(request: NextRequest) {
+  try {
+    const { products, CustomError } = await getProductsByTag('men')
+    if (CustomError) throw new Error(JSON.stringify(CustomError))
+    return NextResponse.json(products, { status: 200, statusText: 'GET OK' })
+  } catch (error) {
+    return NextResponse.json(error, { status: 500, statusText: 'GET ERROR: Internal Server Error' })
+  }
+}
+//* =====================================================================================
