8775c79f8c9debacc25f60913c83c6759dd6f45c
Newman Ferrer <newmanferrer@gmail.com>
2023-03-31 15:33:18 -0400

    refactor: :recycle: refactor className, styles and image format to webp
    refactor className, styles and image format to webp
    
    issue - 310323 - refactor className, styles and image format to webp

--- app/components/SectionBrands/SectionBrands.tsx
+++ app/components/SectionBrands/SectionBrands.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './SectionBrands.module.scss'
-import adidasImage from '@/public/images/products/adidas/adidas-section-brand.jpg'
+import adidasImage from '@/public/images/products/adidas/adidas-section-brand.webp'
 import githubImage from '@/public/images/products/github/github-section-brand.webp'
 import nikeImage from '@/public/images/products/nike/nike-section-brand.webp'
 
@@ -16,19 +16,15 @@ export async function SectionBrands({ title, subtitle }: IProps) {
   return (
     <section className={styles.container}>
       <div className={styles.container__title_and_subtitle_wrapper}>
-        <h2 className={styles.container__title_and_subtitle_wrapper__title}>{title}</h2>
-        <h3 className={styles.container__title_and_subtitle_wrapper__subtitle}>{subtitle}</h3>
+        <h2 className={styles.container__title}>{title}</h2>
+        <h3 className={styles.container__subtitle}>{subtitle}</h3>
       </div>
 
       <div className={styles.container__brands_wrapper}>
-        <div className={styles.container__brands_wrapper__adidas_wrapper}>
-          <Link
-            className={styles.container__brands_wrapper__adidas_wrapper__link}
-            href='/products/adidas'
-            title='go to adidas'
-          >
+        <div className={styles.container__adidas_wrapper}>
+          <Link className={styles.container__link} href='/products/adidas' title='go to adidas'>
             <Image
-              className={styles.container__brands_wrapper__adidas_wrapper__link}
+              className={`${styles.container__image} ${styles.container__image_blur}`}
               src={adidasImage}
               alt='adidas image'
               width={400}
@@ -37,30 +33,23 @@ export async function SectionBrands({ title, subtitle }: IProps) {
           </Link>
         </div>
 
-        <div className={styles.container__brands_wrapper__github_wrapper}>
-          <Link
-            className={styles.container__brands_wrapper__github_wrapper__link}
-            href='/products/github'
-            title='go to github'
-          >
+        <div className={styles.container__github_wrapper}>
+          <Link className={styles.container__link} href='/products/github' title='go to github'>
             <Image
-              className={styles.container__brands_wrapper__github_wrapper__link__image}
+              className={`${styles.container__image} ${styles.github_wrapper__image}`}
               src={githubImage}
               alt='github image'
               width={400}
               height={400}
+              placeholder='blur'
             />
           </Link>
         </div>
 
-        <div className={styles.container__brands_wrapper__nike_wrapper}>
-          <Link
-            className={styles.container__brands_wrapper__nike_wrapper__link}
-            href='/products/nike'
-            title='go to nike'
-          >
+        <div className={styles.container__nike_wrapper}>
+          <Link className={styles.container__link} href='/products/nike' title='go to nike'>
             <Image
-              className={styles.container__brands_wrapper__nike_wrapper__link__image}
+              className={`${styles.container__image} ${styles.nike_wrapper__image}`}
               src={nikeImage}
               alt='nike image'
               width={400}
