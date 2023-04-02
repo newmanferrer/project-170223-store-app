f5e671ec1e278278a362de211bb8db04a3292046
Newman Ferrer <newmanferrer@gmail.com>
2023-04-01 21:31:53 -0400

    refactor: :recycle: delete test components "NavBarTest" and "GlowingCornersEffect"
    delete test components "NavBarTest" and "GlowingCornersEffect"
    
    issue -010423 - delete test components "NavBarTest" and "GlowingCornersEffect"

--- app/components/GlowingCornersEffect/GlowingCornersEffect.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './GlowingCornersEffect.module.scss'
-
-export function GlowingCornersEffect() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.container__div}></div>
-    </div>
-  )
-}
--- app/components/index.ts
+++ app/components/index.ts
@@ -5,7 +5,6 @@ export { Logo } from './Logo/Logo'
 
 export { Navigation } from './Navigation/Navigation'
 export { NavigationMobile } from './NavigationMobile/NavigationMobile'
-export { NavBarTest } from './NavBarTest/NavBarTest'
 
 export { HamburgerIcon } from './HamburgerIcon/HamburgerIcon'
 export { CloseIcon } from './CloseIcon/CloseIcon'
@@ -28,5 +27,3 @@ export { ProductDetailsCard } from './ProductDetailsCard/ProductDetailsCard'
 export { ShoppingCart } from './ShoppingCart/ShoppingCart'
 export { CartProductCard } from './CartProductCard/CartProductCard'
 export { AddToCartButton } from './AddToCartButton/AddToCartButton'
-
-export { GlowingCornersEffect } from './GlowingCornersEffect/GlowingCornersEffect'
