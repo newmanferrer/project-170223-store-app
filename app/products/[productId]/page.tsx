e17f386d644c6bbad149a5023f815dd70cd44e4c
Newman Ferrer <newmanferrer@gmail.com>
2023-04-05 01:21:51 -0400

    feat: :sparkles: created function "generateMetadata"
    created function "generateMetadata"
    
    issue - 050423 - created function "generateMetadata"

--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -2,14 +2,19 @@ import { services } from '@/services'
 import { Header, ProductDetailsCard } from '@/app/components'
 import styles from './page.module.scss'
 
-export const metadata = {
-  title: 'Product Details Page',
-  description: 'Product Details Page'
-}
-
 interface IProps {
   params: {
     productId: string
+  }
+}
+
+export async function generateMetadata({ params }: IProps) {
+  const productId = params.productId
+  const product = await services.getProductById(productId)
+
+  return {
+    title: product.name,
+    description: 'Product Details Page'
   }
 }
 
