6426ddfe9fae2f447b45032e7cead918c6527fd9
Newman Ferrer <newmanferrer@gmail.com>
2023-04-19 12:30:36 -0400

    refactor: :recycle: refactor and include "PrismaAdapter"
    refactor and include "PrismaAdapter"
    
    issue - 190423 - refactor and include "PrismaAdapter"

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,41 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from '@/lib'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const adapter = PrismaAdapter(prisma)
+
+export const providers = [
+  GithubProvider({
+    clientId: `${process.env.GITHUB_ID}`,
+    clientSecret: `${process.env.GITHUB_SECRET}`
+  })
+]
+
+export const pages = {
+  signIn: '/auth/signin',
+  signOut: '/auth/signout',
+  error: '/auth/error',
+  verifyRequest: '/auth/verify-request',
+  newUser: '/auth/new-user'
+}
+
+export const authOptions = {
+  adapter,
+  providers,
+  pages
+}
+
 export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  const providers = [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!
-    })
-  ]
-
-  const isDefaultSigninPage = req.method === 'GET' && req.query.nextauth?.includes('signin')
-
-  //* Will hide the `GoogleProvider` when you visit `/api/auth/signin`
-  if (isDefaultSigninPage) providers.pop()
-
-  return await NextAuth(req, res, { providers })
+  return await NextAuth(req, res, {
+    ...authOptions,
+    callbacks: {
+      async signIn({ user, account, profile, email, credentials }) {
+        const isAllowedToSignIn = true
+        if (isAllowedToSignIn) return true
+        else return false
+      }
+    }
+  })
 }
-
-/* export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    })
-  ]
-}
-
-export default NextAuth(authOptions) */
