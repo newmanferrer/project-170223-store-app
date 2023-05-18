c4af715d96c66fdba44450e85c4ef1c3f657beed
Newman Ferrer <newmanferrer@gmail.com>
2023-05-18 16:34:07 -0400

    refactor: :recycle: add tag main for children property
    add tag main for children property
    
    issue 180523 - add tag main for children property

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: IProps) {
       <head />
       <body>
         <GlobalContextsProviders>
-          {children}
+          <main>{children}</main>
           <ShoppingCart />
           <ScrollToTopButton />
           <Footer />
