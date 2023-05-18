685dad97b9f77cd4f32a35b1e174a445d9472eb7
Newman Ferrer <newmanferrer@gmail.com>
2023-05-18 16:38:16 -0400

    refactor: :recycle: refactor api route to simplify and leave alone "Credentials Provider"
    refactor api route to simplify and leave alone "Credentials Provider"
    
    issue 180523 - refactor api route to simplify and leave alone "Credentials Provider"

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,77 @@
-import NextAuth from 'next-auth'
-import GithubProvider from 'next-auth/providers/github'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { prisma } from '@/lib'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import type { NextAuthOptions } from 'next-auth'
-
-export const adapter = PrismaAdapter(prisma)
-
-export const providers = [
-  GithubProvider({
-    clientId: `${process.env.GITHUB_ID}`,
-    clientSecret: `${process.env.GITHUB_SECRET}`
-  })
-]
-
-export const pages = {
-  signIn: '/auth/signin',
-  signOut: '/auth/signout',
-  error: '/auth/error',
-  verifyRequest: '/auth/verify-request',
-  newUser: '/auth/new-user'
-}
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { prisma } from '@/lib/prisma/db/client'
+import { compare } from 'bcrypt'
 
 export const authOptions: NextAuthOptions = {
-  adapter,
-  providers,
-  pages
+  session: {
+    strategy: 'jwt'
+  },
+  pages: {
+    signIn: '/signin'
+  },
+  providers: [
+    CredentialsProvider({
+      name: `"credentials provider"`,
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'hello@example.com'
+        },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email }
+        })
+
+        if (!user) {
+          return null
+        }
+
+        const isPasswordValid = await compare(credentials.password, user.password)
+
+        if (!isPasswordValid) {
+          return null
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user.image
+        }
+      }
+    })
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id
+        }
+      }
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        return {
+          ...token,
+          id: user.id
+        }
+      }
+
+      return token
+    }
+  }
 }
 
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  return await NextAuth(req, res, {
-    ...authOptions,
-    callbacks: {
-      async signIn({ user, account, profile, email, credentials }) {
-        const isAllowedToSignIn = true
-        if (isAllowedToSignIn) return true
-        else return false
-      },
-      async redirect({ url, baseUrl }) {
-        //* Allows relative callback URLs
-        if (url.startsWith('/')) return `${baseUrl}${url}`
-        //* Allows callback URLs on the same origin
-        else if (new URL(url).origin === baseUrl) return url
-        return baseUrl
-      },
-      async jwt({ token, account, profile, user }) {
-        if (account) {
-          token.id = user.id
-          token.accessToken = account.access_token
-          token.email = profile?.email
-        }
-        return token
-      },
-      async session({ session, token, user }) {
-        //* Send properties to the client, like an access_token and user id from a provider.
-        return session
-      }
-    }
-  })
-}
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
